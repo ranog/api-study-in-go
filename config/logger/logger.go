028ccc8967d5c404a512fca1256b8c8a6ff024a1
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-06-16 02:06:29 -0300

    chore: hide a password type field from user data

--- config/logger/logger.go
+++ config/logger/logger.go
@@ -9,3 +9,17 @@ func InitLogger() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 }
+
+type User struct {
+	Name     string `json:"name"`
+	Age      int    `json:"age"`
+	Password string `json:"password"`
+}
+
+func (u User) LogUser() slog.Value {
+	return slog.GroupValue(
+		slog.String("name", u.Name),
+		slog.Int("age", u.Age),
+		slog.String("password", "HIDDEN"),
+	)
+}
--- config/logger/logger_test.go
+++ config/logger/logger_test.go
@@ -42,3 +42,42 @@ func TestInitLogger_StartingAPI(t *testing.T) {
 		t.Fatalf("\nCurrent:   %s\nExpected: %s", expectedResult, result)
 	}
 }
+
+func TestUser_LogUser(t *testing.T) {
+	timeNow := time.Now()
+	r, w, _ := os.Pipe()
+	originalStdout := os.Stdout
+	os.Stdout = w
+
+	user := logger.User{
+		Name:     "John",
+		Age:      30,
+		Password: "123456",
+	}
+
+	logger.InitLogger()
+	slog.Info("Starting API", slog.Time("time", timeNow), "user", user.LogUser())
+
+	w.Close()
+	os.Stdout = originalStdout
+
+	var buf bytes.Buffer
+	buf.ReadFrom(r)
+	var resultMap map[string]interface{}
+	json.Unmarshal([]byte(buf.String()), &resultMap)
+
+	result, _ := json.Marshal(resultMap)
+
+	payload := map[string]interface{}{
+		"time":  timeNow.Format("2006-01-02T15:04:05.999999999-07:00"),
+		"level": "INFO",
+		"msg":   "Starting API",
+		"user":  map[string]any{"age": 30, "name": "John", "password": "HIDDEN"},
+	}
+
+	expectedResult, _ := json.Marshal(payload)
+
+	if !bytes.Equal(result, expectedResult) {
+		t.Fatalf("\nCurrent:  %s\nExpected: %s", expectedResult, result)
+	}
+}
