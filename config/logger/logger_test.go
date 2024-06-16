932f7c5b3942c140fa280ebd91529dc0ea96357b
Joao.Nogueira <jprnogueira@yahoo.com.br>
2024-06-15 21:54:30 -0300

    fix: test

--- config/logger/logger_test.go
+++ config/logger/logger_test.go
@@ -12,17 +12,10 @@ import (
 )
 
 func TestInitLogger_StartingAPI(t *testing.T) {
+	timeNow := time.Now()
 	r, w, _ := os.Pipe()
 	originalStdout := os.Stdout
 	os.Stdout = w
-
-	timeNow := time.Now()
-	payload := map[string]interface{}{
-		"time":  timeNow.Format("2006-01-02T15:04:05.999999999-07:00"),
-		"level": "INFO",
-		"msg":   "Starting API",
-	}
-	expectedResult, _ := json.Marshal(payload)
 
 	logger.InitLogger()
 	slog.Info("Starting API", slog.Time("time", timeNow))
@@ -34,9 +27,18 @@ func TestInitLogger_StartingAPI(t *testing.T) {
 	buf.ReadFrom(r)
 	var resultMap map[string]interface{}
 	json.Unmarshal([]byte(buf.String()), &resultMap)
+
 	result, _ := json.Marshal(resultMap)
 
-	if !bytes.Equal(expectedResult, result) {
-		t.Fatalf("\nExpected: %s\nCurrent:   %s", expectedResult, result)
+	payload := map[string]interface{}{
+		"time":  timeNow.Format("2006-01-02T15:04:05.999999999-07:00"),
+		"level": "INFO",
+		"msg":   "Starting API",
+	}
+
+	expectedResult, _ := json.Marshal(payload)
+
+	if !bytes.Equal(result, expectedResult) {
+		t.Fatalf("\nCurrent:   %s\nExpected: %s", expectedResult, result)
 	}
 }
